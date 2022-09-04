c9e7656c00049c642ccae8489974ee6ebb8790cd
jamesbaroi <james.a.baroi@gmail.com>
2022-09-04 09:52:43 -0700

    Portfolio

--- src/public/js/alt.js
+++ src/public/js/alt.js
@@ -1,9 +1,20 @@
-/* Timestamp */
+/**Timestamp */
 
 const ts = document.getElementById('timestamp');
 
 if (ts) {
 
   ts.innerHTML = new Date;
+
+}
+
+/**Scroll up */
+function scrUp() {
+
+  /**For Safari */
+  document.body.scrollTop = 0;
+
+  /**For Chrome, Firefox, IE and Opera */
+  document.documentElement.scrollTop = 0;
 
 }
